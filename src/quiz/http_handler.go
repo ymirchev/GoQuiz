05ac055dd5f2fba6710381d3912cd9a35db81a2a
yuri <urimir@gmail.com>
2018-09-06 14:39:07 +0300

    Refactoring

--- src/quiz/http_handler.go
+++ src/quiz/http_handler.go
@@ -19,19 +19,23 @@ type InteractionHandler struct {
 }
 
 func (h InteractionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("handle_interactive_event()")
+	if ok, msg := h.isValidRequest(w, r); ok {
+		h.handleUserAction(w, r, msg)
+	}
+}
 
+func (h InteractionHandler) isValidRequest(w http.ResponseWriter, r *http.Request) (bool, *slack.AttachmentActionCallback) {
 	if r.Method != http.MethodPost {
 		log.Printf("[ERROR] Invalid method: %s", r.Method)
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
+		return false, nil
 	}
 
 	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("[ERROR] Failed to read request body: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return false, nil
 	}
 
 	jsonStr, err := url.QueryUnescape(string(buf)[8:])
@@ -39,34 +43,40 @@ func (h InteractionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("[ERROR] Failed to unespace request body: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return false, nil
 	}
 
 	var message slack.AttachmentActionCallback
 	if err := json.Unmarshal([]byte(jsonStr), &message); err != nil {
 		log.Printf("[ERROR] Failed to decode json message from slack: %s", jsonStr)
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return false, nil
 	}
 
 	// Only accept message from slack with valid token
 	if message.Token != h.VerificationToken {
 		log.Printf("[ERROR] Invalid token: %s", message.Token)
 		w.WriteHeader(http.StatusUnauthorized)
-		return
+		return false, nil
 	}
 
 	// Only accept user reaction on the current and relevant action
-	if UsersData.UsersCount() == 0 ||  message.CallbackID != UsersData.GetCallbackID(message.User.ID) {
+	if UsersData.UsersCount() == 0 || message.CallbackID != UsersData.GetCallbackID(message.User.ID) {
 		fmt.Println("not_allowed_user_action")
 		w.WriteHeader(http.StatusConflict)
-		return
+		return false, nil
 	}
 
-	uid := message.User.ID
-	action := message.Actions[0]
+	return true, &message
+}
 
-	origMsg := message.OriginalMessage
+func (h InteractionHandler) handleUserAction(w http.ResponseWriter, r *http.Request, msg *slack.AttachmentActionCallback) {
+	fmt.Println("handleUserAction()")
+
+	uid := msg.User.ID
+	action := msg.Actions[0]
+	origMsg := msg.OriginalMessage
+
 	var attachment slack.Attachment
 	var cid string
 
@@ -82,7 +92,7 @@ func (h InteractionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 
 	case ActionStartQuiz:
-		if UsersData.GetCategory(uid) != "" && UsersData.GetQuestionNum(uid) != ""{
+		if UsersData.GetCategory(uid) != "" && UsersData.GetQuestionNum(uid) != "" {
 			qd := GetQuiz(UsersData.GetCategory(uid), UsersData.GetQuestionNum(uid))
 			UsersData.SetQuizData(uid, *qd)
 
